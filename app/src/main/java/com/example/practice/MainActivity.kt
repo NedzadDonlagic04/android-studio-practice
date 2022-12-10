c64dd56a507311286a2d931d387f43a56b05027f
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-11 00:21:42 +0100

    Finished project

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counterBtn: Button = findViewById(R.id.counterBtn)
+        val counterText: TextView = findViewById(R.id.counter)
+        var counter = 0
+
+        counterBtn.setOnClickListener {
+            counter++
+            counterText.text = counter.toString()
+        }
     }
 }
